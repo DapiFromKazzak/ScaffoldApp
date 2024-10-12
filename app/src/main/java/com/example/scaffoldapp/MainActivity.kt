70f9935e4edbfc6eacb02e7bb7bda0475c4d39ed
DapiFromKazzak <aaro.kallioinen@gmail.com>
2024-10-12 19:59:27 +0300

    Tehtävä valmis

---
+++ app/src/main/java/com/example/scaffoldapp/MainActivity.kt
@@ -0,0 +1,165 @@
+package com.example.scaffoldapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.scaffoldapp.ui.theme.ScaffoldAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ScaffoldAppTheme {
+                ScaffoldApp()
+            }
+        }
+    }
+}
+
+@Composable
+fun ScaffoldApp(){
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = "Home"
+    ){
+        composable(route = "Home"){
+            MainScreen(navController)
+        }
+        composable(route = "Info"){
+            InfoScreen(navController)
+        }
+        composable(route = "Settings"){
+            SettingsScreen(navController)
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    ScaffoldAppTheme {
+        ScaffoldApp()
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainTopBar(title: String, navController: NavController){
+    var expanded by remember { mutableStateOf(false) }
+
+    TopAppBar(
+        title = { Text(title) },
+        actions = {
+            IconButton(
+                onClick = {
+                    expanded = !expanded
+                }
+            ) {
+                Icon(Icons.Filled.MoreVert, contentDescription = null)
+            }
+            DropdownMenu(
+                expanded = expanded,
+                onDismissRequest = { expanded = false}
+            ) {
+                DropdownMenuItem(
+                    text = { Text(text = "Info") },
+                    onClick = { navController.navigate("Info")
+                })
+                DropdownMenuItem(
+                    text = { Text(text = "Settings") },
+                    onClick = { navController.navigate("Settings")
+                })
+
+            }
+        }
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ScreenTopBar(title: String, navController: NavController){
+    TopAppBar(
+        title = { Text(title) },
+        navigationIcon = {
+            IconButton(onClick = { navController.navigateUp()}) {
+                Icon(Icons.AutoMirrored.Filled.ArrowBack, contentDescription = null)
+            }
+        }
+    )
+}
+
+@Composable
+fun MainScreen(navController: NavController) {
+    Scaffold(
+        topBar = { MainTopBar("My App", navController) }
+    ){ innerPadding ->
+        Column(
+            modifier = Modifier.padding(innerPadding)
+        ){
+            Text(text = "Content for Home screen")
+        }
+
+    }
+}
+
+@Composable
+fun InfoScreen(navController: NavController) {
+    Scaffold(
+        topBar = { ScreenTopBar("Info", navController) }
+    ){ innerPadding ->
+        Column(
+            modifier = Modifier.padding(innerPadding)
+        ){
+            Text(text = "Content for Info screen")
+        }
+
+    }
+}
+
+@Composable
+fun SettingsScreen(navController: NavController) {
+    Scaffold(
+        topBar = { ScreenTopBar("Settings", navController) }
+    ){ innerPadding ->
+        Column(
+            modifier = Modifier.padding(innerPadding)
+        ){
+            Text(text = "Content for Settings screen")
+        }
+
+    }
+}
